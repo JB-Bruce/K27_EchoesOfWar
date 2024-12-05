2f6535d76e9749b29ef7982ce89340b646c14484
wighthood <zefires@gmail.com>
2024-12-05 17:29:08 +0100

    blocking player movement, need to block rotation

--- Assets/Scripts/PaperMap.cs
+++ Assets/Scripts/PaperMap.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
-public class PaperMap : MonoBehaviour, Interactable
+public class PaperMap : MonoBehaviour, IInteractable
 {
     [SerializeField] private Transform target;
     public void Interact()
     {
         Camera.main.transform.GetComponent<TestCameraScript>().target = target;
     }
+
+    public bool DoesNeedToStopPlayerMovement { get; } = true;
 }
