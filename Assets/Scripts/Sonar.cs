3d213594e012fea6fb3d6123a0d5659fddf14eb8
JB-Bruce <B.JEAN-BAPTISTE@lahorde.school>
2024-12-06 10:52:23 +0100

    finished sonar

--- Assets/Scripts/Sonar.cs
+++ Assets/Scripts/Sonar.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,59 +8,92 @@ public class Sonar : MonoBehaviour
     public Texture2D texture; // La texture que vous voulez afficher
 
     private Rect currentUVRect; // La zone UV visible de la texture
-    private float zoomSpeed = 0.1f; // Vitesse de zoom
-    private float panSpeed = 0.01f; // Vitesse de déplacement
+
+    [SerializeField] bool doesLoop;
+    [SerializeField] List<ZoomInfo> sizes = new();
+    int curIndex;
 
     void Start()
     {
-        if (rawImage == null || texture == null)
-        {
-            Debug.LogError("Assurez-vous que RawImage et Texture sont assignées !");
-            return;
-        }
-
         rawImage.texture = texture;
 
         currentUVRect = new Rect(0, 0, 1, 1);
         rawImage.uvRect = currentUVRect;
+
+        HandleZoom();
     }
 
     void Update()
     {
-        HandleZoom();
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0)
+        {
+            if(scroll < 0)
+                ZoomIn();
+            else 
+                ZoomOut();
+        }
         HandlePan();
     }
 
     void HandleZoom()
     {
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if (scroll != 0)
+
+        float nextZoom = sizes[curIndex].zoom;
+
+        currentUVRect.width = Mathf.Clamp(nextZoom, 0, 1);
+        currentUVRect.height = Mathf.Clamp(nextZoom, 0, 1);
+
+        currentUVRect.x = Mathf.Clamp(currentUVRect.x, 0, 1 - currentUVRect.width);
+        currentUVRect.y = Mathf.Clamp(currentUVRect.y, 0, 1 - currentUVRect.height);
+
+        rawImage.uvRect = currentUVRect;
+
+    }
+
+    public void ZoomIn()
+    {
+        SetNextZoom(true);
+        HandleZoom();
+    }
+
+    public void ZoomOut()
+    {
+        SetNextZoom(false);
+        HandleZoom();
+    }
+
+    private void SetNextZoom(bool next)
+    {
+        int increase = next ? 1 : -1;
+
+        curIndex = curIndex + increase;
+
+        if (curIndex >= sizes.Count)
         {
-            float zoomFactor = 1 - scroll * zoomSpeed;
-
-            float minZoom = 0.1f;
-            float maxZoom = 1f;
-
-            currentUVRect.width = Mathf.Clamp(currentUVRect.width * zoomFactor, minZoom, maxZoom);
-            currentUVRect.height = Mathf.Clamp(currentUVRect.height * zoomFactor, minZoom, maxZoom);
-
-            currentUVRect.x = Mathf.Clamp(currentUVRect.x, 0, 1 - currentUVRect.width);
-            currentUVRect.y = Mathf.Clamp(currentUVRect.y, 0, 1 - currentUVRect.height);
-
-            rawImage.uvRect = currentUVRect;
+            if (doesLoop) curIndex %= sizes.Count;
+            else curIndex = sizes.Count - 1;
+        }
+        else if (curIndex < 0)
+        {
+            if (doesLoop) curIndex = sizes.Count + curIndex;
+            else curIndex = 0;
         }
     }
 
     void HandlePan()
     {
-
-        //currentUVRect.x = Mathf.Clamp(currentUVRect.x + panX, 0, 1 - currentUVRect.width);
-        //currentUVRect.y = Mathf.Clamp(currentUVRect.y + panY, 0, 1 - currentUVRect.height);
-
         currentUVRect.x = (MapManager.testVector.position.x / 3000f) - 0.5f * (currentUVRect.width);
         currentUVRect.y = (MapManager.testVector.position.y / 3000f) - 0.5f * (currentUVRect.height);
 
         rawImage.uvRect = currentUVRect;
 
+    }
+
+    [System.Serializable]
+    public struct ZoomInfo
+    {
+        public string length;
+        public float zoom;
     }
 }
