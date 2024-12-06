5dadd7470aba4aeed0821864d646874801c79d8c
wighthood <zefires@gmail.com>
2024-12-06 16:54:23 +0100

    made a method to call the screen shake coroutine
    if one's already running it stop it and start again

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CameraScript : MonoBehaviour
 {
@@ -8,30 +9,41 @@ public class CameraScript : MonoBehaviour
     [SerializeField] private float _rotationSpeed = 10f;
     [SerializeField] private float _zoomSpeed = 10f;
 
+    IEnumerator CurrentShakeCoroutine;
     private void Start()
     {
         cameraTransform = transform;
+        startShake(0.5f, .25f);
     }
     
     /// <summary>
-    /// Coroutine that make the camera shake
+    /// start the coroutine that shake the screen
     /// </summary>
-    /// <param name="duration">duration of the shake</param>
-    /// <param name="magnitude">intensity of the shake, the higher it more intense the shake is</param>
-    /// <returns></returns>
-    public IEnumerator CameraShake(float duration, float magnitude)
+    /// <param name="duration">time duration of screen shake</param>
+    /// <param name="magnitude">intensity of the screen shake, the higher the farther the screen shake</param>
+    public void startShake(float duration, float magnitude)
+    {
+        if (CurrentShakeCoroutine != null)
+            StopCoroutine(CurrentShakeCoroutine);
+        CurrentShakeCoroutine = CameraShake(duration, magnitude);
+        StartCoroutine(CurrentShakeCoroutine);
+    }
+    
+    private IEnumerator CameraShake(float duration, float magnitude)
     {
         Vector3 startPos = cameraTransform.localPosition;
+        
         float elapsed = 0f;
+
         while (elapsed < duration)
         {
-            float x = Random.Range(-1, 1)*magnitude;
-            float y = Random.Range(-1, 1)*magnitude;
-            cameraTransform.localPosition = new Vector3( x, y, startPos.z);
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+            cameraTransform.localPosition = new Vector3(x, y, cameraTransform.localPosition.z);
             elapsed += Time.deltaTime;
             yield return null;
         }
-        cameraTransform.localPosition = startPos;
+        yield return null;
     }
     
     private void Update()
