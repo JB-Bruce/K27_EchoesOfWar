97a9712cdeb0e40789d57c2acbcbfab5af5cf6dd
LucyanaPanda <lucie.nguyenpham@gmail.com>
2024-12-13 18:56:46 +0100

    Update

--- Assets/Scripts/Wiring Puzzle/Wire.cs
+++ Assets/Scripts/Wiring Puzzle/Wire.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,14 +9,18 @@ public class Wire : MonoBehaviour
     public Transform End;
 
     private Camera _camera;
+    private float CameraZDistance;
     private readonly UnityEvent _linked = new();
 
     private Vector3 _initPos;
     private Vector3 _initScale;
 
+    private bool isDragging = false;
+
     void Awake()
     {
         _camera = Camera.main;
+        CameraZDistance = _camera.WorldToScreenPoint(transform.position).z;
         _transform = GetComponent<Transform>();
 
         _initPos = transform.localPosition;
@@ -23,33 +28,48 @@ public class Wire : MonoBehaviour
     }
     private void Update()
     {
-       
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+        } 
+        else
+        {
+            isDragging = false;
+        }
+
+        if(isDragging)
+        {
+
+        } 
+        else
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            if (ray.Equals(End))
+            {
+                _linked.Invoke();
+            }
+            else
+            {
+                ResetWire();
+            }
+        }
     }
+
     private void ResetWire()
     {
         _transform.position = _initPos;
         _transform.right = Vector3.zero;
+        _transform.localScale = _initScale;
     }
 
     private void OnMouseDrag()
     {
-        Vector3 NewPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mouseScreenPosition = new Vector3(_transform.position.x, _transform.position.y, CameraZDistance);
+        Vector3 NewPosition = _camera.ScreenToWorldPoint(mouseScreenPosition);
         _transform.position = NewPosition;
         Debug.Log("hfo");
     }
 
-    private void OnMouseDown()
-    {
-        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-        if (ray.Equals(End))
-        {
-            _linked.Invoke();
-        }
-        else
-        {
-            ResetWire();
-        }
-    }
 
     public UnityEvent Linked => _linked;
 }
