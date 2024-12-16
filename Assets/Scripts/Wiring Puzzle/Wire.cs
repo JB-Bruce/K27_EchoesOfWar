5449bb2886d9f1a11b672cd9d47d7aead938f436
LucyanaPanda <lucie.nguyenpham@gmail.com>
2024-12-16 17:31:11 +0100

    Update Wiring

--- Assets/Scripts/Wiring Puzzle/Wire.cs
+++ Assets/Scripts/Wiring Puzzle/Wire.cs
@@ -1,75 +1,22 @@
-using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UIElements;
 
 public class Wire : MonoBehaviour
 {
-    [SerializeField] private Transform _transform;
     public Transform End;
-
-    private Camera _camera;
-    private float CameraZDistance;
-    private readonly UnityEvent _linked = new();
-
-    private Vector3 _initPos;
-    private Vector3 _initScale;
-
-    private bool isDragging = false;
+    public Transform Transform;
+    public bool IsLinked = false;
+    [HideInInspector] public Vector3 InitPos;
+    [HideInInspector] public Vector3 InitScale;
+    [HideInInspector] public float InitZ;
+    
 
     void Awake()
     {
-        _camera = Camera.main;
-        CameraZDistance = _camera.WorldToScreenPoint(transform.position).z;
-        _transform = GetComponent<Transform>();
-
-        _initPos = transform.localPosition;
-        _initScale = transform.localScale;
+        InitPos = transform.localPosition;
+        InitScale = transform.localScale;
+        InitZ = Camera.main.WorldToScreenPoint(transform.position).z;
     }
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            isDragging = true;
-        } 
-        else
-        {
-            isDragging = false;
-        }
-
-        if(isDragging)
-        {
-
-        } 
-        else
-        {
-            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-            if (ray.Equals(End))
-            {
-                _linked.Invoke();
-            }
-            else
-            {
-                ResetWire();
-            }
-        }
-    }
-
-    private void ResetWire()
-    {
-        _transform.position = _initPos;
-        _transform.right = Vector3.zero;
-        _transform.localScale = _initScale;
-    }
-
-    private void OnMouseDrag()
-    {
-        Vector3 mouseScreenPosition = new Vector3(_transform.position.x, _transform.position.y, CameraZDistance);
-        Vector3 NewPosition = _camera.ScreenToWorldPoint(mouseScreenPosition);
-        _transform.position = NewPosition;
-        Debug.Log("hfo");
-    }
-
-
-    public UnityEvent Linked => _linked;
 }
