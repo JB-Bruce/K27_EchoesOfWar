42cc3962310660960a0385cb12b47a71b0e8e3a3
wighthood <zefires@gmail.com>
2024-12-18 10:54:59 +0100

    proper thrust lock

--- Assets/Scripts/Submarine/ThrusterLever.cs
+++ Assets/Scripts/Submarine/ThrusterLever.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Lumin;
 
 [RequireComponent(typeof(Outline))]
 public class ThrusterLever : MonoBehaviour, IFinishedInteractable, IBreakdownReceiver
@@ -41,7 +42,7 @@ public class ThrusterLever : MonoBehaviour, IFinishedInteractable, IBreakdownRec
         _outline = GetComponent<Outline>();
         _outline.enabled = true;
 
-        SetThrusterPosition();
+        SetThrusterPosition(_thrust);
     }
     private void Start()
     {
@@ -61,18 +62,26 @@ public class ThrusterLever : MonoBehaviour, IFinishedInteractable, IBreakdownRec
         _thrust += _movementSpeed * Mathf.Sign(_movementDirection) * Time.deltaTime;
         _thrust = Mathf.Clamp(_thrust, 0, 1);
         
-        if(_thrust > _upperthreshold || _thrust < _lowerthreshold)
-            SetThrusterPosition();
+
+        if (_thrust < _upperthreshold && _thrust > _lowerthreshold)
+        {
+            _transform.localPosition = _origin.localPosition;
+            _transform.localRotation = _origin.localRotation;
+        }
+        else
+        {
+            SetThrusterPosition(_thrust);
+        }
 
         /*_thrust += _movementSpeed * Mathf.Sign(_movementDirection) * Time.deltaTime;
         _thrust = Mathf.Clamp(_thrust, -_distMinToOrigin, _distMaxToOrigin);
         _transform.localPosition = new Vector3(0, 0, _thrust);*/
     }
 
-    private void SetThrusterPosition()
+    private void SetThrusterPosition(float thrust)
     {
-        _transform.localPosition = Vector3.Lerp(_min.localPosition, _max.localPosition, _thrust);
-        _transform.rotation = Quaternion.Slerp(_min.localRotation, _max.localRotation, _thrust);
+        _transform.localPosition = Vector3.Lerp(_min.localPosition, _max.localPosition, thrust);
+        _transform.rotation = Quaternion.Slerp(_min.localRotation, _max.localRotation, thrust);
     }
 
     public void SetMovement(float direction)
