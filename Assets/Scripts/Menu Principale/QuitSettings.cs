ea4bbd00ab893be0d8c29ff9493ed2de4bf8605b
yann985 <fou125kangourou@gmail.com>
2024-12-18 17:32:25 +0100

    fin Menu

--- Assets/Scripts/Menu Principale/QuitSettings.cs
+++ Assets/Scripts/Menu Principale/QuitSettings.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class QuitSettings : MonoBehaviour
 {
+    public GameObject _vidéoFons;
     public GameObject _MenuSittings;
     public void QuitSittings()
     {
+        _vidéoFons.SetActive(true);
         _MenuSittings.SetActive(false);
     } 
 }
--- Assets/Scripts/Menu Principale/Sittings.cs
+++ Assets/Scripts/Menu Principale/Sittings.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class Sittings : MonoBehaviour
 {
     public GameObject _MenuSittings;
+     public GameObject _vidéoFons;
+
+   
+    [SerializeField] VideoPlayer _videoPlays;
+    [SerializeField] VideoClip _videoPclip;
+
+    
     public void StarteSittings()
     {
+        _videoPlays.clip=_videoPclip;
+        _videoPlays.Play();
+
+        _vidéoFons.SetActive(false);
         _MenuSittings.SetActive(true);
     } 
 
